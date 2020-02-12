09799f796f279fe41911acb8f6715cc91544595c
Marcin Wróbel <wrobel.sparrow@gmail.com>
2020-02-12 10:49:31 +0100

    System Paths Contants class added

--- src/main/java/org/example/controller/ContractorController.java
+++ src/main/java/org/example/controller/ContractorController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import org.example.utilities.ContractorsReaderFromCSVFile;
 import org.example.utilities.ListPrinter;
+import org.example.utilities.SystemPathsConstants;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,6 @@ public class ContractorController {
 
     @GetMapping("/list")
     public String getContractorsList() {
-        return ListPrinter.getStringRepresentation(ContractorsReaderFromCSVFile.getContractors("\\src\\main\\resources\\data\\contractors.csv"));
+        return ListPrinter.getStringRepresentation(ContractorsReaderFromCSVFile.getContractors(SystemPathsConstants.CSV_FILE_PATH));
     }
 }
---
+++ src/main/java/org/example/utilities/SystemPathsConstants.java
@@ -0,0 +1,6 @@
+package org.example.utilities;
+
+public class SystemPathsConstants {
+
+    public static final String CSV_FILE_PATH = "\\src\\main\\resources\\data\\contractors.csv";
+}
