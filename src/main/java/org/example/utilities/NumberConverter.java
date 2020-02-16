feb14b8ed59be9deb55c1bc490bc32278cdd6809
Marcin Wróbel <wrobel.sparrow@gmail.com>
2020-02-16 19:46:47 +0100

    NumberConverter refactoring

--- src/main/java/org/example/utilities/NumberConverter.java
+++ src/main/java/org/example/utilities/NumberConverter.java
@@ -1,39 +1,73 @@
 package org.example.utilities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberConverter {
 
+    private static String[][] numeralTable = new String[][]{
+            {"", "jedenaście ", "dwanaście ", "trzynaście ", "czternaście ", "piętnaście ", "szesnaście ", "siedemnaście ", "osiemnaście ", "dziewiętnaście "},
+            {"", "jeden ", "dwa ", "trzy ", "cztery ", "pięć ", "sześć ", "siedem ", "osiem ", "dziewięć "},
+            {"", "dziesięć ", "dwadzieścia ", "trzydzieści ", "czterdzieści ", "pięćdziesiąt ", "sześćdziesiąt ", "siedemdziesiąt ", "osiemdziesiąt ", "dziewięćdziesiąt "},
+            {"", "sto ", "dwieście ", "trzysta ", "czterysta ", "pięćset ", "sześćset ", "siedemset ", "osiemset ", "dziewięćset "},
+            {"", "tysiąc ", "tysiące ", "tysięcy "}
+    };
+
     public static String getAmountInWords(String amount) {
-
-        String[][] orderOfMagnitudeWordsTable = new String[][]{
-                {"", "jeden ", "dwa ", "trzy ", "cztery ", "pięć ", "sześć ", "siedem ", "osiem ", "dziewięć "},
-                {"", "jedenaście ", "dwanaście ", "trzynaście ", "czternaście ", "piętnaście ", "szesnaście ", "siedemnaście ", "osiemnaście ", "dziewiętnaście "},
-                {"", "dziesięć ", "dwadzieścia ", "trzydzieści ", "czterdzieści ", "pięćdziesiąt ", "sześćdziesiąt ", "siedemdziesiąt ", "osiemdziesiąt ", "dziewięćdziesiąt "},
-                {"", "sto ", "dwieście ", "trzysta ", "czterysta ", "pięćset ", "sześćset ", "siedemset ", "osiemset ", "dziewięćset "},
-                {"", "tysiąc ", "tysiące ", "tysięcy "}
-        };
-
         String[] split = amount.split(",");
         int orderOfMagnitude = split[0].length();
-        int[] orderOfMagnitudeArray = new int[orderOfMagnitude];
         int integerValue = Integer.parseInt(split[0]);
-        int decimalValue = Integer.parseInt(split[1]);
+        String decimalValue = split[1];
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < orderOfMagnitude; i++) {
-            orderOfMagnitudeArray[i] = integerValue % 10;
-            integerValue /= 10;
+        Map<Integer, Integer> map = new HashMap<>();
+        int tempValue = integerValue;
+
+        for (int i = 1; i < orderOfMagnitude + 1; i++) {
+            map.put(i, tempValue % 10);
+            tempValue /= 10;
         }
-        for (int i = orderOfMagnitude - 1; i >= 0; i--) {
-            if (i > 1) {
-                result.append(orderOfMagnitudeWordsTable[i + 1][orderOfMagnitudeArray[i]]);
-            } else {
-                if (orderOfMagnitudeArray[i] > 1 && i == 1) {
-                    result.append(orderOfMagnitudeWordsTable[i + 1][orderOfMagnitudeArray[i]]);
-                } else {
-                    result.append(orderOfMagnitudeWordsTable[i][orderOfMagnitudeArray[i]]);
+
+        for (int i = orderOfMagnitude; i > 0; i--) {
+            if (orderOfMagnitude > 1) {
+                switch (i) {
+
+                    case 1:
+                        if (map.get(2) == 1 && map.get(i) != 0) {
+                            result.append(numeralTable[0][map.get(i)]);
+                        } else {
+                            result.append(numeralTable[i][map.get(i)]);
+                        }
+                        break;
+
+                    case 2:
+                        if (map.get(i) == 1 && map.get(1) != 0) {
+                            break;
+                        } else {
+                            result.append(numeralTable[i][map.get(i)]);
+                        }
+                        break;
+
+                    case 3:
+                        result.append(numeralTable[i][map.get(i)]);
+                        break;
+
+                    case 4:
+                        if (map.get(i) < 5) {
+                            if (map.get(i) == 1) {
+                                result.append(numeralTable[1][1] + numeralTable[4][1]);
+                            } else {
+                                result.append(numeralTable[1][map.get(i)] + numeralTable[4][2]);
+                            }
+                        } else {
+                            result.append(numeralTable[1][map.get(i)] + numeralTable[4][3]);
+                        }
+                        break;
                 }
+            } else {
+                result.append(numeralTable[i][map.get(i)]);
             }
         }
-        result.append("złotych " + decimalValue + "/100 groszy");
+        result.append("złotych " + decimalValue + "/100");
         return result.toString();
     }
 }
